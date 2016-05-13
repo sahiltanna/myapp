a8a4a5d34b5fcb32d0fcc5a04262272250feadb0
stl-anand-dodia <anand.dodia@solutelabs.com>
2016-05-13 12:04:13 +0530

    first commit

---
+++ database/migrations/2016_05_13_063150_create_User_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('User', function (Blueprint $table) {
+            
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
